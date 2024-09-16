81e53fad51f386bbd1dbedf26e349036dae1914a
Demetris Stavrou <1180929+demestav@users.noreply.github.com>
2024-09-16 23:07:03 +0300

    Icons saved in a XDG spec compatible directory

--- src/tabler_icons/__init__.py
+++ src/tabler_icons/__init__.py
@@ -9,4 +9,10 @@ from django.conf import settings
 if "DJANGO_SETTINGS_MODULE" in os.environ and hasattr(settings, "TABLER_ICONS_DIR"):
     icon_directory = pathlib.Path(settings.TABLER_ICONS_DIR)
 else:
-    icon_directory = pathlib.Path.home() / ".config" / "django-tabler-icons" / "icons"
+    icon_directory = (
+        pathlib.Path(
+            os.environ.get("XDG_DATA_HOME", pathlib.Path.home() / ".local" / "share")
+        )
+        / "django-tabler-icons"
+        / "icons"
+    )
